6e989f07791b0930d968ef2e79e3fca2b4902c1b
HeCodes2Much <wayne6324@gmail.com>
2019-08-18 21:20:44 +0100

    fixed

--- lang/german/custom/systems/system-chestSystem.js
+++ lang/german/custom/systems/system-chestSystem.js
@@ -12,11 +12,11 @@
  *
  */
 
-$.lang.register('chestsystem.chest.usage', 'Usage: ~chest [edit | toggle].');
+$.lang.register('chestsystem.chest.usage', 'Usage: !chest [edit | toggle].');
 $.lang.register('chestsystem.toggle.announce', 'Live Toggle has been $1.');
 //chest edit command.
-$.lang.register('chestsystem.edit.usage', 'Usage: ~chest edit <chest> <MinNumber> <MaxNumber>.');
-$.lang.register('chestsystem.edit.usage.exists', 'Usage: ~chest edit <chest> <MinNumber> <MaxNumber>. $1 Current setting: Min: $2, Max: $3.');
+$.lang.register('chestsystem.edit.usage', 'Usage: !chest edit <chest> <MinNumber> <MaxNumber>.');
+$.lang.register('chestsystem.edit.usage.exists', 'Usage: !chest edit <chest> <MinNumber> <MaxNumber>. $1 Current setting: Min: $2, Max: $3.');
 $.lang.register('chestsystem.edit.usage.pointsystem', 'chests can\'t be edited, because module "$1" isn\'t enabled.');
 $.lang.register('chestsystem.edit.success-new', 'Added new $1 chest for Min: $2, Max: $3.');
 $.lang.register('chestsystem.edit.success-update', 'Updated $1 chest for Min: $2, Max: $3.');
